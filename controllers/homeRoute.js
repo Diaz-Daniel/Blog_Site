ca143ace79d9bf5498180dd412dd7f588565b9b1
DDiaz <daniel.diaz.0515@gmail.com>
2021-11-29 21:47:23 -0800

    connected route from blogpost to main

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 const { User } = require("../models");
 
+router.get("/", (req, res) => {
+  //check database for any posts
+  //if posts, display on page
+  res.render("blogPost");
+});
+
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/");
