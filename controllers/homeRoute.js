6225e9e9b82b1141b6e57e61a6bbdc10fb9ff34f
DDiaz <daniel.diaz.0515@gmail.com>
2021-11-30 20:53:51 -0800

    Buttons on homepage send user to login when user not logged in

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,15 +1,36 @@
 const router = require("express").Router();
-const { User } = require("../models");
+const { User, Blog } = require("../models");
 
-router.get("/", (req, res) => {
-  //check database for any posts
-  //if posts, display on page
-  res.render("blogPost");
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const blog = blogData.map((blog) => blog.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("homepage", {
+      blog,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect("/");
+    res.redirect("/dashboard");
     return;
   }
 
