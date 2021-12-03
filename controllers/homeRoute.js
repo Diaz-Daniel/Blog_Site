b152474c3364ffe8ca9ab8e964f9fc2667e9ff57
DDiaz <daniel.diaz.0515@gmail.com>
2021-12-03 11:51:01 -0800

    working out issues with homepage/dashboard render

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -28,6 +28,28 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/blog/:id", async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+
+    const blog = blogData.get({ plain: true });
+
+    res.render("homepage", {
+      ...blog,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/dashboard");
