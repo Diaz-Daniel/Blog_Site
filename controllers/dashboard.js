b8a5ab5a544b7635bff4832b8141dca09c384d0e
DDiaz <daniel.diaz.0515@gmail.com>
2021-12-16 20:16:35 -0800

    get route for edit created

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -20,12 +20,24 @@ router.get("/", withAuth, async (req, res) => {
   }
 });
 
+//need to build out route
 router.get("/new", withAuth, (req, res) => {
   res.render("new", { logged_in: req.session.logged_in });
 });
 
-router.get("/edit/:id", withAuth, (req, res) => {
-  res.render("edit", { logged_in: req.session.logged_in });
+//need to build out route
+router.get("/edit/:id", withAuth, async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const blogData = await Blog.findByPk(req.params.id);
+
+    // Serialize data so the template can read it
+    const oneblog = blogData.get({ plain: true });
+
+    res.render("edit", { oneblog, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
